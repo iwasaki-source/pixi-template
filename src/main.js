b264b3ee4cae1a9ebaa328865921fef784504a40
iwasaki <iwa@cfv.co.jp>
2024-06-06 17:32:43 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.scss'
+import { Application } from 'pixi.js';
+
+(async () => {
+  const app = new Application()
+
+  await app.init({ background: '#1099bb', resizeTo: window });
+
+  document.body.appendChild(app.canvas);
+})();
